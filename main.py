ca51e87ef135c66ca1855e7642e1679960441382
Živilė <63965345+stunze@users.noreply.github.com>
2020-12-31 20:14:49 +0200

    Update main.py
    Kol kas viska surasau i du baitus, reiks optimizuoti ir dar nelabai patikrinau ar tikrai viska uzkoduoja, nes itartinai nedidelis failas gaunasi...

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
+MAX_DICT_SIZE = 4096
+
+def init_dictionary():
+    return {format(i, "08b"): i for i in range(256)}
+
+
+def check_dictionary(dictionary: dict, element: str):  # reik nepamirst daugiau checku uzdet
+    if len(dictionary) < MAX_DICT_SIZE:
+        return True
+
+
+def bytes_from_file(filename):
+    with open(filename, "rb") as input_stream:
+        while True:
+            byte = input_stream.read(1)
+            if not byte:
+                break
+            yield byte
+
+
+def find_in_dictionary(dictionary: dict, key):
+    for k, v in dictionary.items():
+        if k == key:
+            return v
+
+
+def encoded_to_file(filename: str, encoded):
+    with open(filename,'wb') as output_stream:
+        output_stream.write(encoded)
+
+
+def encoder(filename_in: str, filename_out: str):
+    dictionary = init_dictionary()
+    code = ""
+    encoded = bytearray()
+
+    for byte in bytes_from_file(filename_in):
+        # if not (check_dictionary(dictionary, code)):
+        #     break
+        if len(code) < 1:
+            code += '{:08b}'.format(ord(byte))
+        elif code in dictionary.keys():
+            value = find_in_dictionary(dictionary, code)
+            code += '{:08b}'.format(ord(byte))
+        else:
+            encoded += value.to_bytes(2, byteorder='big')
+            dictionary.update({code: len(dictionary)})
+            code = ""
+    print(encoded)
+    encoded_to_file(filename_out, encoded)
+
 def read_from_file(filename: str):
     # perskaitom abeceles dydi
     try:
